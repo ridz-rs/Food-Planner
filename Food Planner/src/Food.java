3198dda536117a5accba49acae4b99deb58d9c87
Riddhesh Shah <riddhesh.shah@mail.utoronto.ca>
2019-12-22 13:42:53 -0500

    Added tracking variable

---
+++ Food Planner/src/Food.java
@@ -0,0 +1,9 @@
+
+public class Food {
+String name;
+double price_limit;
+public Food(String name, double price) {
+	this.name = name;
+	this.price_limit = price;
+}
+}
--- Food Planner/src/PlannerModel.java
+++ Food Planner/src/PlannerModel.java
@@ -1,4 +1,5 @@
-import java.util.HashMap;
+// import java.util.HashMap;
+import java.util.*;
 public class PlannerModel extends Observable{
-
+HashMap <String, ArrayList<Food>> planned_meals = new HashMap<String, ArrayList<Food>>();
 }
