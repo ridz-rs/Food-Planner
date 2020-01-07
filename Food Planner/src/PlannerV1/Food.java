67c505829185512b3cf3ffe9c7679023c737112a
Riddhesh Shah <riddhesh.shah@mail.utoronto.ca>
2020-01-06 19:47:17 -0500

    Datainput solved

--- Food Planner/src/PlannerV1/Food.java
+++ Food Planner/src/PlannerV1/Food.java
@@ -2,9 +2,17 @@ package PlannerV1;
 
 public class Food {
 String name;
-double price_limit;
-public Food(String name, double price) {
+double price;
+double calories;
+public Food(String name, double price, double calories) {
 	this.name = name;
-	this.price_limit = price;
+	this.price = price;
+	this.calories = calories;
+}
+public Food (String name) {
+	this.name = name;
+}
+public void set_price(double price) {
+	this.price = price;
 }
 }
--- Food Planner/src/PlannerV1/PlannerModel.java
+++ Food Planner/src/PlannerV1/PlannerModel.java
@@ -1,17 +1,84 @@
 package PlannerV1;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.io.*;
 public class PlannerModel extends Observable{
-HashMap <String, ArrayList<Food>> planned_meals = new HashMap<String, ArrayList<Food>>();
-static HashMap <String, ArrayList<Food>> data = new HashMap<String, ArrayList<Food>>();
-public static void get_data(String filename) { //To be changed
-	PizzaPizza pizzapizza = new PizzaPizza();
-	pizzapizza.add(new Food("Pepperoni Slice", 3.99));
-	pizzapizza.add(new Food("Cheese Slice", 3.99));
-	pizzapizza.add(new Food("Chicken Slice", 4.59));
-	pizzapizza.add(new Food("Meat Lovers Slice", 4.59));
-	pizzapizza.add(new Food("Veggie Medley Slice", 4.59));
-	pizzapizza.add(new Food("Super Plant Slice", 3.99));
-	pizzapizza.add(new Food("Pepperoni Slice", 3.99));
-}
+	private Pattern pRestaurantStart = Pattern.compile("(\\w*),*");
+	private Pattern pNoSizeStart = Pattern.compile("Name,Calories,Price");
+	private Pattern pNoSizeData = Pattern.compile("(\\w+\\s*\\w*),(\\d\\d?\\d?\\d?),\\$?(\\d\\.?\\d?\\d?)");
+	private Pattern pNoSizeCombos = Pattern.compile("(.*,),(\\$\\d\\.?\\d?\\d?)");
+	private Pattern pRestaurantEnd = Pattern.compile(",*");
+	HashMap <String, ArrayList<Food>> planned_meals = new HashMap<String, ArrayList<Food>>();
+	static HashMap <String, ArrayList<Food>> data = new HashMap<String, ArrayList<Food>>();
+	public boolean get_data(String filename) { //To be changed
+		FileReader f = null;
+		try {
+		 f = new FileReader(filename);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		BufferedReader bf = new BufferedReader(f);
+		String l;
+		int state = 0;
+		//String curr_rest = null;
+		ArrayList<Food> food = new ArrayList<Food>();
+		try {
+			while ((l= bf.readLine())!=null) {
+				switch(state) {
+				case 0:
+					Matcher m = pRestaurantStart.matcher(l);
+					if (m.matches()) {
+						state=1;
+						food = new ArrayList<Food>();
+						data.put(m.group(1), food);
+						//curr_rest = m.group(0);
+					}
+					else { 
+						System.out.print("Not Reading Title");
+						state =99;
+					}
+					break;
+				case 1:
+					if (pNoSizeStart.matcher(l).matches()) state=2;
+					else {
+						System.out.println("Start format wrong");
+						state=99;
+					}
+					break;
+				case 2:
+					m = pNoSizeData.matcher(l);
+					if (m.matches()) {
+						String name = m.group(1);
+						double calories = Double.valueOf(m.group(2));
+						double price = Double.valueOf(m.group(3));
+						food.add(new Food(name,price, calories));
+						state = 2; 
+					}
+					else {
+						if (pRestaurantEnd.matcher(l).matches()) state =0;
+						else {
+							state=99;
+							System.out.println("DataFormat wrong"+l);
+						}
+					}
+					break;
+				case 99:
+					break;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (state!=99) return true;
+		else return false;
+	}
+	public static void main(String args[]) {
+		String filename = "C:\\Users\\Riddhesh\\Downloads\\PizzaPizzaData.csv";
+		PlannerModel model = new PlannerModel();
+		System.out.println(model.get_data(filename));
+		System.out.println(model.data.toString());
+	}
 }
