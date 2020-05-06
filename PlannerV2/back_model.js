86a52b0ba66f1377b4bce69df81829ac51da8875
Riddhesh Shah <riddhesh.shah@mail.utoronto.ca>
2020-05-06 05:24:19 -0400

    creating food graph

--- PlannerV2/back_model.js
+++ PlannerV2/back_model.js
@@ -1,3 +1,20 @@
+class Graph{
+    constructor(length){
+        this.adjlist = [];
+        this.length = length;
+    }
+    hash(key, length){
+        index = 0;
+        for(let i=0; i<key.length(); i++){
+            index += key.charCodeAt(i);
+        }
+        return index%length;
+    }
+    add_node(name, price, calories, food_group){
+        index = hash(name, length);
+        adjlist[index] = FoodNode(price, calories,food_group);
+    }
+}
 class FoodNode{
     constructor(price, calories, food_group){
         this.price = price;
@@ -14,5 +31,5 @@ class Edge{
         this.source = source;
         this.destination = destination;
         this.discount = discount;
-    }
+    }   
 }
