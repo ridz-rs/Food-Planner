37cb4883304a060584fe54fde74a72c151c36051
Riddhesh Shah <riddhesh.shah@mail.utoronto.ca>
2020-07-17 04:47:23 -0400

    now reads till baked goods till crashing. Then counter is flawed.

--- PlannerV2/scrapper.py
+++ PlannerV2/scrapper.py
@@ -16,18 +16,12 @@ class timsbot:
         self.data_dict = {}
         
     def baked_goods(self):
-        # sleep(5)
-        # print("starts")
-        # for i in first:
-        #     i.click()
-        #     sleep(5)
             length = 0
             options1 = self.driver.find_elements_by_class_name('taggedNutCalCatProd') # [donuts,timbits,muffins,bakedgoods]
             option1texts = [opt.text for opt in options1]
             print(option1texts)
             options1_count = 0
             for option in options1:
-                # to_click1 = WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Muffins')]")))
                 try:
                     print("goes in 1st try")
                     to_click1 = WebDriverWait(self.driver, 10).\
@@ -45,31 +39,28 @@ class timsbot:
                 option2_count = 0
                 sleep(5)
                 for option2 in options2:
-                    to_click2 = WebDriverWait(self.driver, 20).\
-                        until(expected_conditions.element_to_be_clickable((By.XPATH, "//div[contains(text(),\'{}\')]"\
-                            .format(inner_option2texts[option2_count]))))
+                    try:
+                        to_click2 = WebDriverWait(self.driver, 20).\
+                            until(expected_conditions.element_to_be_clickable((By.XPATH, "//div[contains(text(),\'{}\')]"\
+                                .format(inner_option2texts[option2_count]))))
+                    except TimeoutException:
+                        to_click = self.driver.find_element_by_xpath("//div[contains(text(), \'{}\')]".format(inner_option2texts[option2_count]))
+                        print(to_click.text)
                     self.driver.execute_script("arguments[0].click()", to_click2)
                     sleep(5)
-                    options3 = self.driver.find_elements_by_class_name('taggedNutCalCatProd') #[AppleFritter, Chocolate dip, Maple Dip...]
-                    option3 = options3[0]
-                    self.driver.execute_script("arguments[0].click()", option3) # clicks on the donut
-                    next_button = WebDriverWait(self.driver, 10).\
-                        until(expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Next')))
-                    while(expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Next'))):
-                        self.data_dict[self.driver.find_element_by_id('search-item-title')]=self.driver.find_element_by_id('cal')
-                        # loads donut data in data_dict
-                        self.driver.execute_script("arguments[0].click()", next_button)
-                        sleep(5)
-                        try:
-                            next_button = WebDriverWait(self.driver, 10).\
-                            until(expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Next')))
-                        except TimeoutException:
+                    """
+                        TODO: call read_inner_options() appropriately when the opened page is the innermost one
+                    """
+                    is_deepest = True
+                    try:
+                        self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/img').click()
+                    except:
+                        print("excepts!")
+                        is_deepest = False
+                    finally:
+                        self.read_inner_items()
+                        if is_deepest:
                             break
-                    self.data_dict[self.driver.find_element_by_id('search-item-title')]=self.driver.find_element_by_id('cal')
-                    self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/img').click() # closes donut page
-                    sleep(5)
-                    length = len(list(self.data_dict.keys()))
-                    print(length)
                     back_button = self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[6]/div[2]/div/div[2]/div/a')
                     self.driver.execute_script("arguments[0].click()", back_button)
                     # options2 = options2[1:]
@@ -95,8 +86,29 @@ class timsbot:
 
     def wait_multiple(self, outer_element, class_name):
         pass
+    def read_inner_items(self):
+        options3 = self.driver.find_elements_by_class_name('taggedNutCalCatProd') #[AppleFritter, Chocolate dip, Maple Dip...]
+        option3 = options3[0]
+        self.driver.execute_script("arguments[0].click()", option3) # clicks on the donut
+        next_button = WebDriverWait(self.driver, 10).\
+            until(expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Next')))
+        while(expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Next'))):
+            self.data_dict[self.driver.find_element_by_id('search-item-title')]=self.driver.find_element_by_id('cal')
+            # loads donut data in data_dict
+            self.driver.execute_script("arguments[0].click()", next_button)
+            sleep(5)
+            try:
+                next_button = WebDriverWait(self.driver, 10).\
+                until(expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Next')))
+            except TimeoutException:
+                break
+        self.data_dict[self.driver.find_element_by_id('search-item-title')]=self.driver.find_element_by_id('cal')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/img').click() # closes donut page
+        sleep(5)
+        length = len(list(self.data_dict.keys()))
+        print(length)        
 
-
+        
 bot = timsbot()
 sleep(5)
 tags = bot.driver.find_elements_by_class_name('taggedNutCalCatProd') # [bakedgoods,breakfast,lunch,beverages]
@@ -117,6 +129,7 @@ for tag in tags:
     else:
         break
     count += 1
+print(bot.data_dict)
 
 
 
