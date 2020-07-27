3113b187a1566cb0db741128c1f0a8b26b0dc890
Riddhesh Shah <riddhesh.shah@mail.utoronto.ca>
2020-07-27 03:45:07 -0400

    scraper goes till lunch

--- PlannerV2/RecursiveTimsbot.py
+++ PlannerV2/RecursiveTimsbot.py
@@ -15,53 +15,58 @@ class timsbot:
         self.data_dict = {}
     
     def data_surfer(self):
-        sleep(3)
+        sleep(2)
         is_deepest = False
         try:  # base case
             self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/img').click()
             is_deepest = True
-            return True
         except:
             options = self.driver.find_elements_by_class_name('taggedNutCalCatProd')
             option_texts = [opt.text for opt in options]
-            option_count = 0
             for i in range(len(option_texts)):
-                self.check_arrow(option_count)
-                sleep(3)
-                options = self.driver.find_elements_by_class_name('taggedNutCalCatProd')
+                self.check_arrow(i)
+                sleep(2)
+                options = self.driver.find_elements_by_class_name('taggedNutCalCatProd') #get categories after scrolling
                 option_texts = [opt.text for opt in options]
-                print(option_texts[option_count], option_count, len(option_texts))
-                sleep(3)
+                print(option_texts[i], i, len(option_texts))
+                sleep(2)
                 try:
                     to_click = WebDriverWait(self.driver, 10)\
                         .until(expected_conditions.element_to_be_clickable\
-                            ((By.XPATH, '//div[contains(text(),\"{0}\")]'.format(option_texts[option_count]))))
-                    self.driver.execute_script("arguments[0].click()", to_click)
+                            ((By.XPATH, '//div[contains(text(),\"{0}\")]'.format(option_texts[i]))))
+                    self.driver.execute_script("arguments[0].click()", to_click) # click on category
                     try:
                         print("recurses")
                         to_break = self.data_surfer() # carries out the same code for inner categories
                         if to_break:
+                            print("breaks", option_texts)
                             break
-                        option_count += 1
                     except Exception as e:
-                        print("error at", option_texts[option_count])
+                        print("error at", option_texts[i])
                         print(e)
                         exit()
                 except Exception as e:
                     print("could not click")
                     print(e)
                     break
-                sleep(3)
+                sleep(2)
+            try:
+                print("looks for back button")
+                back_button = self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[6]/div[2]/div/div[2]/div/a')
+                self.driver.execute_script('arguments[0].click()', back_button) # goes back from the items page 
+            except:
+                print("no back button")
+                return False
             print("Goes in outermost except")
-
         finally:
             if is_deepest:
                 self.read_inner_elements()
+                return True
 
     
     def check_arrow(self, num_completed):
         print("checks for arrow")
-        if num_completed==3:
+        if num_completed>=3:
             try:
                 self.driver.find_element_by_class_name("bx-next").click()
                 print("clicks on arrow")
@@ -72,7 +77,7 @@ class timsbot:
         print("reader called")
         options3 = self.driver.find_elements_by_class_name('taggedNutCalCatProd') #[AppleFritter, Chocolate dip, Maple Dip...]
         option3 = options3[0]
-        sleep(3)
+        sleep(2)
         try:
             self.driver.execute_script("arguments[0].click()", option3) # clicks on the donut
         except:
@@ -84,24 +89,30 @@ class timsbot:
                     (expected_conditions.element_to_be_clickable((By.LINK_TEXT,'Next')))
             except TimeoutException:
                 print("could not find next button")
-                break
-            self.data_dict[self.driver.find_element_by_id('search-item-title')]=self.driver.find_element_by_id('cal')
+                self.data_dict[self.driver.find_element_by_id('search-item-title').text] = self.driver.find_element_by_id('cal').text
+                self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/img').click() # closes donut page
+                length = len(list(self.data_dict.keys()))
+                print(length)
+                return None
+            self.data_dict[self.driver.find_element_by_id('search-item-title').text]=self.driver.find_element_by_id('cal').text
             # loads donut data in data_dict
-            self.driver.execute_script("arguments[0].click()", next_button)
-            sleep(3)
-        self.data_dict[self.driver.find_element_by_id('search-item-title')] = self.driver.find_element_by_id('cal')
-        self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/img').click() # closes donut page
-        try:
-            print("looks for back button")
-            back_button = self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[6]/div[2]/div/div[2]/div/a')
-            self.driver.execute_script('arguments[0].click()', back_button)
-        except:
-            print("no back button")
-        sleep(3)
-        length = len(list(self.data_dict.keys()))
-        print(length)
+            if next_button is not None:
+                self.driver.execute_script("arguments[0].click()", next_button)
+            sleep(2)
+
 
 bot = timsbot()
-bot.data_surfer()   
+try:
+    bot.data_surfer()
+except Exception as e:
+    print(e)   
+print(bot.data_dict)
+try:
+    with open('TimsData.csv', 'a') as fd:
+        for key in self.data_dict:
+            fd.writelines([key, '', self.data_dict[key]])
+except Exception as e:
+    print('Writing error')
+    print(e)
 
 
